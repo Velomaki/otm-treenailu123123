6daf6bb5737d4904fc2aa9610523535b187002b7
Velomaki <velomaki@gmail.com>
2018-04-16 03:11:24 +0300

    jacoco ja scrnshot

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,50 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void alkuSaldoOikein() {
+        assertEquals(10,kortti.saldo());      
+    }
+    
+    @Test
+    public void latausOnnistuu() {
+        kortti.lataaRahaa(5);
+        assertEquals(15,kortti.saldo());      
+    }
+    
+    @Test
+    public void ostoOnnistuu() {
+        kortti.otaRahaa(5);
+        assertEquals(5,kortti.saldo());      
+    }
+    
+    @Test
+    public void saldoEiMeneMiinukselle() {
+        kortti.otaRahaa(5555555);
+        assertEquals(10,kortti.saldo());      
+    }
+    
+    @Test
+    public void metodiPalauttaaTrueKunOnnistuu() {
+        //kortti.otaRahaa(5);
+        assertTrue(kortti.otaRahaa(5));      
+    }
+    
+    @Test
+    public void metodiPalauttaaFalseKunFailaa() {
+        //kortti.otaRahaa(5);
+        assertFalse(kortti.otaRahaa(555555));      
+    }
+    
+    @Test
+    public void toStringToimii(){
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    
+    
+    
+    
+    
 }
